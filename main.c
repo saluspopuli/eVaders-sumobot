3a7c78e5b353d6e9e43e1329809bbdea13971026
saluspopuli <andrecortez539@gmail.com>
2023-10-20 14:26:05 +0800

    Added line_sensor_check function

--- main.c
+++ main.c
@@ -17,24 +17,48 @@ int main()
 	// POLULU SETUP ================================================
 	int length = 1000;
 	int turnSpeed = 100;
+	
+	const int sensorTimeOut = 2000;
 	unsigned int sensorVals[5];
 	
-	pololu_3pi_init(2000);
+	pololu_3pi_init(sensorTimeOut);
 	// POLULU SETUP ================================================
 	
 	while(1)
 	{
-		turnLeftBig(500, 100);
-		turnRightBig(500, 100);
+		clear();
+		lcd_goto_xy(0,0);
+		print_long(line_sensor_check(sensorVals,sensorTimeOut));
+		delay_ms(1000);
 	}
 }
 
 // ================== FUNCTIONS ===================================
 
-int line_sensor_check(unsigned int *sensorVals){
+// LINE_SENSOR_CHECK
+// Returns an integer value from -20 to 20 which indicates the difference in
+// IR light between the 5 on-board IR sensors (middle sensor is unused here).
+// The integer range can be used to determine whether or not the robot should go
+// left or right whenever it encounters uneven light.
+int line_sensor_check(unsigned int *sensorVals, int sensorTimeOut ){
 	read_line_sensors(sensorVals, IR_EMITTERS_ON);
+	unsigned int tmp[5];
+	int sum, tmpDivisor;
 	
-	// Traverse a 5 element array
+	tmp[0] = -sensorVals[0];
+	tmp[1] = -sensorVals[1];
+	tmp[2] = 0;
+	tmp[3] = sensorVals[3];
+	tmp[4] = sensorVals[4];
+	
+	for (int i = 0; i < 5; i++){
+		sum += tmp[i];
+	}
+	
+	tmpDivisor = sensorTimeOut/10;
+	sum = (sum/tmpDivisor)-1;
+	
+	return sum;
 }
 
 
